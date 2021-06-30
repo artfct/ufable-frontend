6114f746189704fc8372b9c0c982e7cbbc03050e
Yesinn Kwak <yesinn.kwak@gmail.com>
2021-06-30 16:34:44 -0400

    add pathing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar/Navbar'
-import Sidenavbar from './components/sidenavbar/Sidenavbar'
-import ClubListFeed from './components/clubListFeed/ClubListFeed'
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+import './App.css';
+import Navbar from './components/clubList/navbar/Navbar';
+import Sidenavbar from './components/clubList/sidenavbar/Sidenavbar';
+import ClubListFeed from './components/clubList/clubListFeed/ClubListFeed';
+import LoginPage from './components/loginPage/LoginPage';
 
 
 function App() {
   return (
-    <div className="main">
-      <Navbar />
-      <Sidenavbar />
-      <ClubListFeed />
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Navbar />
+            <Sidenavbar/>
+            <ClubListFeed/>
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
 
-    </div>
+        </Switch>
+      </BrowserRouter>
+    </>
+
   );
 }
 
