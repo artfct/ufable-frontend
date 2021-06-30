2cd510a6091e37ef21bd89fedb22909b0c1d69d8
artfct <dae.jung@mail.utoronto.ca>
2021-06-30 15:49:50 -0400

    test commit for new branch

---
+++ src/components/clubListFeed/ClubListFeed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClubListFeed = () => {
+    return (
+        <div>
+            this is the club lsit feed
+        </div>
+    )
+}
+
+export default ClubListFeed
---
+++ src/components/sidenavbar/Sidenavbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidenavbar = () => {
+    return (
+        <div>
+            this is side nav bar
+        </div>
+    )
+}
+
+export default Sidenavbar
